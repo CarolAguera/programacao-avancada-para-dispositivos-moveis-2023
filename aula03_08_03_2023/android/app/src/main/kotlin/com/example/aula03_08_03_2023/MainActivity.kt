0cf319ca0c72dca72ca971b4a6e8e529937a42d8
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2023-03-10 22:14:13 -0300

    Delete aula03_08_03_2023 directory

--- aula03_08_03_2023/android/app/src/main/kotlin/com/example/aula03_08_03_2023/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.aula03_08_03_2023
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- aula03_08_03_2023/lib/ui/widgets/widgets.dart
+++
@@ -1,2 +0,0 @@
-//exporta para importacao todos os arquivos listados abaixo
-export 'campo_texto.dart';
